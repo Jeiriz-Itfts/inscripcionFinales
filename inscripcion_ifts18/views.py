05dd28d08273b23f0c09add55434df15c1c9cf06
Juan Cruz Eiriz <juan.eiriz@alu.ifts18.edu.ar>
2021-11-27 12:43:09 -0300

    agrego vistas correspondientes a inscripcionFinales, logout

--- inscripcion_ifts18/views.py
+++ inscripcion_ifts18/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
-from .models import Usuario
 from django.utils import timezone
 from django.urls import reverse
 from django.core.mail import EmailMultiAlternatives
@@ -8,32 +7,53 @@ from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 from django.template.context import RequestContext
 from django import forms
+from .models import Alumno, Usuario
 from .forms import EmailForm
 import uuid
 from django.template.loader import get_template
+
 #get_template de template loader
 
-def sendMail(mail):
+def inscripcionFinales(request):
+    return render(request,'inscripcion_ifts18/alumno/inscripcion.html')
+
+def logout(request):
+    return render(request,'inscripcion_ifts18/alumno/logout.html')
+
+
+
+def sendMail(mail, newPassword):
+    context = {'newPassword': newPassword}
     template = get_template('inscripcion_ifts18/correo.html')
-    content = template.render({}) #esto hace que sea dinamico y pasarle un valor que permite utilizar en el template, esa vairable, ese valor que le paso
+    content = template.render(context) #esto hace que sea dinamico y pasarle un valor que permite utilizar en el template, esa vairable, ese valor que le paso
 
     email = EmailMultiAlternatives(
                   'IFTS 18 - Reseteo automático de contraseña',
-                  'Se ha creado una nueva contraseña',
                   settings.EMAIL_HOST_USER,
                   [mail]
                   )
     email.attach_alternative(content, "text/html") #atach el correo en el template html y el formato
-    email.send() #envia correo
+    try:
+        email.send() #envia correo
+    except:
+        return HttpResponse("Error al enviar correo")
 
 def check_if_mail_exists_and_send_mail(request):
     if request.method == 'POST':
         mail = request.POST.get('mail')
         if mail:
-            if Usuario.objects.filter(mail=mail).exists():
-                newPassword=uuid.uuid4()
-                sendMail(mail)
-            return HttpResponse("El correo no se encuentra registrado!")
+            try:
+                #hacer que no se puedan ingresar usuarios duplicados
+                Usuario.objects.filter(mail=mail).exists()
+                usuario = Usuario.objects.get(mail=mail)
+                if usuario.mail:
+                    newPassword = str(uuid.uuid4())
+                    usuario.password = newPassword
+                    usuario.save()
+                    sendMail(mail, newPassword)
+                    return render(request, 'inscripcion_ifts18/reseteado.html')
+            except Usuario.DoesNotExist:
+                return HttpResponse("El correo no se encuentra registrado!")
         return HttpResponse("No se ha introducido ningún correo")
     return render(request, 'inscripcion_ifts18/reset.html')
 
@@ -45,7 +65,13 @@ def check(request):
         if mail:
             password = request.POST.get('password')
             if Usuario.objects.filter(mail=mail).exists() & Usuario.objects.filter(password=password).exists():
-                return render(request, 'inscripcion_ifts18/logged.html')
+                usuario = Usuario.objects.get(mail=mail)
+                id = usuario.id
+                """ mail duplicado """
+                if Alumno.objects.filter(id_usuario=id).exists():
+                    alumno = Alumno.objects.get(id_usuario=id)
+                    return render(request, 'inscripcion_ifts18/alumno/index.html', {'nombre': alumno.nombre})
+                return HttpResponse("El usuario no es alumno")
             return HttpResponse("El usuario con el mail %s no existe" % mail)
         return HttpResponse("No se ha introducido ningún correo")
     return HttpResponse("404")
