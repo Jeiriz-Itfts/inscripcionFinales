d98ebd192e6f00b5c7b823484910352b3e8f0db7
Juan Cruz Eiriz <juan.eiriz@alu.ifts18.edu.ar>
2021-12-07 09:31:48 -0300

    agrego model en seriazer que permite pegarle a la api

--- inscripcion_ifts18/serializer.py
+++ inscripcion_ifts18/serializer.py
@@ -5,3 +5,8 @@ class UsuarioSeriazer (ModelSerializer):
     class Meta:
         model = Usuario
         fields = '__all__'
+
+class MateriaAlumnoSeriazer (ModelSerializer):
+    class Meta:
+        model = MateriaAlumno
+        fields = '__all__'
